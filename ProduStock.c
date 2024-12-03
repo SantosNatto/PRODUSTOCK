8174f5c3a5926d2b06c71dded84f61ea40bda1b6
Renato Santos <renatosanttosoficial@gmail.com>
2024-12-03 15:58:47 -0300

    Add files via upload

---
+++ ProduStock.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+
+void CadastroProduto();
+
+int main(){
+
+    return 0;
+}
+
+void CadastroProduto(){
+    int cod;
+    char nome[50], descricao[100], fornecedor[30];
+
+    printf("")
+
+}
